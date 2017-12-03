f2f8991b098c83a7e5e7371f3c6f7c42484f0c4c
Jsx <saurabh@rebugged.com>
2017-12-03 20:59:36 +0530

    Initial commit, basic working prototype

---
+++ README.md
@@ -0,0 +1,31 @@
+# `gxa-exercise` — search for pubclications in Europe PMC
+
+See the number of publications that match the given query in Europe PMC for a given year range. If year range is not provided, data for the last 10 years is fetched.
+
+## Prerequisites
+
+- Node.js
+- Yarn
+- Bower
+
+## Development
+
+To run the project, do this:
+
+```
+git https://github.com/riverspirit/gxa-exercise.git
+cd gxa-exercise
+yarn start
+```
+
+
+## Testing
+To be added
+
+## Running in Production
+To be aded
+
+## To Do
+- Build process
+- Tests
+- ESLint
---
+++ app/components/highcharts-bar/highcharts-bar-directive.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('myApp.highchartsBar-directive', [])
+
+  .controller('highchartsBarCtrl', ['$scope', function ($scope) {
+    $scope.chartOptions = {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: ''
+      },
+      xAxis: {
+          categories: $scope.data.years,
+          crosshair: true
+      },
+      yAxis: {
+          min: 0,
+          title: {
+              text: 'Number of publications'
+          }
+      },
+      tooltip: {
+          headerFormat: '<span style="color:{series.color};padding:0">Year:</span> <span><b>{point.key}</b></span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">Publications: </td>' +
+              '<td style="padding:0"><b>{point.y}</b></td></tr>' +
+              '<tr><td style="color:{series.color};padding:0">Most cited: </td>' +
+              '<td style="padding:0"><b>{point.mostCited}</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      series: [{
+        type: 'column',
+        data: $scope.data.data,
+        showInLegend: false
+    }]
+  };
+
+  }])
+
+  .directive('highchartsBar', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: '='
+      },
+      controller: 'highchartsBarCtrl',
+      template: '<div style="min-width: 310px; height: 400px; margin: 0 auto"></div>',
+      link: function (scope, element) {
+        scope.$watch('data', function (newData, oldData) {
+          if (newData && newData.years && newData.data) {
+            if (!scope.chart) {
+              scope.chart = Highcharts.chart(element[0], scope.chartOptions);
+            } else {
+              scope.chart.xAxis[0].setCategories(newData.years, false);
+              scope.chart.series[0].setData(newData.data, false);
+              scope.chart.redraw();
+            }
+          }
+        });
+      },
+    };
+  });
